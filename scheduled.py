91dfd256f908ae9125fb1c9370d8ef55c9b310ef
zerosy22 <92256238+zerosy22@users.noreply.github.com>
2024-05-28 12:29:24 +0900

    scheduler 실습

---
+++ scheduled.py
@@ -0,0 +1,41 @@
+# 사용자가 직접 실행하는 DAG를 생성 코드 입니다.
+
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id="scheduled", start_date=datetime(year=2023, month=1, day=1), end_date=datetime(year=2023, month=1, day=5)
+, schedule_interval="@daily",
+)
+
+fetch_events = BashOperator(
+    task_id="fetch_events",
+    bash_command=(
+        "curl -o /tmp/events.json http://13.209.16.219:5000/events"
+        # 본인 ec2 웹서버 주소로 변경해야함
+    ),
+    dag=dag,
+)
+
+def _calculate_stats(input_path, output_path):
+
+    Path(output_path).parent.mkdir(exist_ok=True)
+
+    events = pd.read_json(input_path)
+    stats = events.groupby(["date", "user"]).size().reset_index()
+
+    stats.to_csv(output_path, index=False)
+
+calculate_stats = PythonOperator(
+    task_id="calculate_stats",
+    python_callable=_calculate_stats,
+    op_kwargs={"input_path": "/tmp/events.json", "output_path": "/tmp/stats.csv"},
+    dag=dag,
+)
+
+fetch_events >> calculate_stats
